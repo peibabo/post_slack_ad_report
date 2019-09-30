e6ada3e67fa14c339b4a3a123b9a6ab1f172d6d8
yuhei_babo <yuhei598@gmail.com>
2019-09-30 14:33:20 +0900

    昨日のレポートも取得対象にする

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,19 @@ import configparser
 import json
 import time
 import requests
+import datetime
+import copy
 
+TODAY = 'today'
+YESTERDAY = 'yesterday'
+SELECT_DAYS = {
+    TODAY:'今日',
+    YESTERDAY:'昨日',
+}
+REPORT_HASH = {
+    TODAY: [],
+    YESTERDAY: [],
+}
 
 def lambda_handler(event, context):
     options = webdriver.ChromeOptions()
@@ -30,13 +42,20 @@ def lambda_handler(event, context):
         "./bin/chromedriver",
         chrome_options=options)
 
-    post_slack("*スマートニュース* " + add_pre_format(smart_news(driver)))
-    post_slack("*SQUADレポート* " + add_pre_format(squad(driver)))
+    # AM8:00に回る場合は昨日レポートも取得
+    dt_now = datetime.datetime.now()
+    yesterday_flag = False
+    if dt_now.hour == 8 and dt_now.minute < 30
+      yesterday_flag = True
+
+
+    post_slack("*スマートニュース* " + add_pre_format(smart_news(driver, yesterday_flag)))
+    post_slack("*SQUADレポート* " + add_pre_format(squad(driver, yesterday_flag)))
 
     driver.close()
     driver.quit()
 
-def smart_news(driver):
+def smart_news(driver, yesterday_flag):
     MEDIA = "sn"
     CAMPAIGN_IDS = [19517007, 12993177]
     COLUMNS = {
@@ -72,46 +91,35 @@ def smart_news(driver):
     # ログイン
     driver.find_element_by_class_name(LOGIN_BUTTON_ELM_NAME).click()
 
-    # キャンペーンごとに回す
-    report_array = []
-    for campaign_id in CAMPAIGN_IDS:
+    report_hash = copy.deepcopy(REPORT_HASH)
 
-        # 目的のページへ遷移
-        driver.get(GOAL_URL + str(campaign_id))
+    # 日付毎に回す
+    for date in SELECT_DAYS:
 
-        driver.find_element_by_id('insights-datepicker').click()
+        # 昨日レポートは指定時間しか取得しない
+        if date == YESTERDAY and yesterday_flag == False
+          continue
 
-        driver.find_element_by_xpath("//li[@data-range-key='今日']").click()
+        # キャンペーンごとに回してレポート取得
+        for campaign_id in CAMPAIGN_IDS:
 
-        time.sleep(1)
+            # 目的のページへ遷移
+            driver.get(GOAL_URL + str(campaign_id))
 
-        html = driver.page_source.encode('utf-8')
-        soup = BeautifulSoup(html, "html.parser")
-        rows = soup.select('.fixedDataTableLayout_rowsContainer .fixedDataTableRowLayout_rowWrapper')
+            driver.find_element_by_id('insights-datepicker').click()
 
-        for i, row in enumerate(rows):
-            if i == 0:
-                continue
-            span = row.select('.public_fixedDataTableCell_cellContent span')
-            if span[COLUMNS['SPENDING']].get_text() == "-":
-                continue
+            report_hash[date].append(parse_smartnews_report(driver, date))
 
-            columns_dict = {}
-            for k, v in COLUMNS.items():
-                columns_dict[k] = span[v].get_text()
+        # 合計spending計算
+        total_spending = 0
+        for report in report_hash[date]:
+            total_spending += convert_str_to_int_money(report['SPENDING'])
 
-            report_array.append(columns_dict)
+        report_hash[date].append({'合計消費' : "{:,}".format(total_spending)+"円"})
 
-    # 合計spending計算
-    total_spending = 0
-    for report in report_array:
-        total_spending += convert_str_to_int_money(report['SPENDING'])
+    return json.dumps(report_hash[TODAY], indent=2, ensure_ascii=False)
 
-    report_array.append({'合計消費' : "{:,}".format(total_spending)+"円"})
-
-    return json.dumps(report_array, indent=2, ensure_ascii=False)
-
-def squad(driver):
+def squad(driver, yesterday_flag):
     MEDIA = "squad"
     COLUMNS = {
         'NAME':2,
@@ -137,13 +145,67 @@ def squad(driver):
     driver.find_element_by_name(ID_ELM_NAME).send_keys(id)
     driver.find_element_by_name(PW_ELM_NAME).send_keys(password)
 
-    # ログイン 
+    # ログイン
     driver.find_element_by_name(LOGIN_BUTTON_ELM_NAME).click()
 
-    # 目的ページに遷移
-    driver.get(GOAL_URL)
+    report_hash = copy.deepcopy(REPORT_HASH)
 
-    driver.find_element_by_xpath("//input[@data-disable-with='今日']").click()
+    # 日付毎に回す
+    for date in SELECT_DAYS:
+
+        # 昨日レポートは指定時間しか取得しない
+        if date == YESTERDAY and yesterday_flag == False
+          continue
+
+        # 目的ページに遷移
+        driver.get(GOAL_URL)
+
+        report_hash[date].append(parse_squad_report(driver, date))
+
+        # 合計reward計算
+        total_reward = 0
+        for report in report_hash[date]:
+            total_reward += convert_str_to_int_money(report['REWARD'])
+
+        report_hash[date].append({'合計報酬' : "{:,}".format(total_reward)+"円"})
+
+    return json.dumps(report_hash[TODAY], indent=2, ensure_ascii=False)
+
+    total_reward = 0
+    for report in report_array:
+        total_reward += convert_str_to_int_money(report['REWARD'])
+
+    report_array.append({'合計報酬' : "{:,}".format(total_reward)+"円"})
+
+    return json.dumps(report_array, indent=2, ensure_ascii=False)
+
+def parse_smartnews_report(driver, target_day):
+    driver.find_element_by_xpath("//li[@data-range-key='"+SELECT_DAYS[target_day]+"']").click()
+
+    time.sleep(1)
+
+    html = driver.page_source.encode('utf-8')
+    soup = BeautifulSoup(html, "html.parser")
+    rows = soup.select('.fixedDataTableLayout_rowsContainer .fixedDataTableRowLayout_rowWrapper')
+
+    report_array = []
+    for i, row in enumerate(rows):
+        if i == 0:
+            continue
+        span = row.select('.public_fixedDataTableCell_cellContent span')
+        if span[COLUMNS['SPENDING']].get_text() == "-":
+            continue
+
+        columns_dict = {}
+        for k, v in COLUMNS.items():
+            columns_dict[k] = span[v].get_text()
+
+        report_array.append(columns_dict)
+
+    return report_array
+
+def parse_squad_report(driver, target_day):
+    driver.find_element_by_xpath("//input[@data-disable-with='"+SELECT_DAYS[target_day]+"']").click()
 
     html = driver.page_source.encode('utf-8')
     soup = BeautifulSoup(html, "html.parser")
@@ -162,13 +224,7 @@ def squad(driver):
 
         report_array.append(columns_dict)
 
-    total_reward = 0
-    for report in report_array:
-        total_reward += convert_str_to_int_money(report['REWARD'])
-
-    report_array.append({'合計報酬' : "{:,}".format(total_reward)+"円"})
-
-    return json.dumps(report_array, indent=2, ensure_ascii=False)
+    return report_array
 
 def post_slack(post_message):
     SLACK_WEBHOOK = "https://hooks.slack.com/services/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"
